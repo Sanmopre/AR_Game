dfbb81c53b19e971ba7b7b678713756f281767c2
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2021-05-26 18:48:01 +0200

    gravity manager script

---
+++ AR_Game_Project/Assets/GravityManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GravityManager : MonoBehaviour
+{
+
+private GameObject[] boxes;
+private Rigidbody2D[] rbs;
+
+    void Start()
+    {
+        boxes = GameObject.FindGameObjectsWithTag("boxes");
+        rbs = new Rigidbody2D[boxes.Length];
+
+        for (int i = 0; i < boxes.Length; ++i)
+        {
+            GameObject door = boxes[i];
+            rbs[i] = door.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    void Update()
+    {
+        ApplyGravity();
+    }
+
+    void ApplyGravity()
+    {
+        foreach (Rigidbody2D rb in rbs)
+        {
+            //Add force perpendicular to the plane
+            //rb.AddForce();
+        }
+    }
+
+}
